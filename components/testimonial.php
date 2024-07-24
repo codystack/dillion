ceb3c70f98b3bc7d1491fd46875bb8b97b5f1009
codystack <ebuzzadvert@gmail.com>
2024-07-24 18:01:07 +0100

    services  added

--- components/testimonial.php
+++ components/testimonial.php
@@ -8,7 +8,7 @@
                 </div>
                 <div class="owl-carousel 3block-carusel owl-outer-20 nav-disable">
                     <div class="col">
-                        <div class="p-35 bg-white transation box-shadow h-100 rounded hover-style-1">
+                        <div class="p-35 bg-white transation box-shadow h-100 rounded1 hover-style-1">
                             <p><i class="flaticon-quote me-1"></i>I can't thank Dillion Property Construction enough for their dedication and expertise in renovating our home. From start to finish, your team was attentive to our needs and ensured every detail was perfect. Thank you.</p>
                             <div class="agent-style-1 d-flex align-items-center mt-30">
                                 <div class="image-wrap">
@@ -23,7 +23,7 @@
                         </div>
                     </div>
                     <div class="col">
-                        <div class="p-35 bg-white transation box-shadow h-100 rounded hover-style-1">
+                        <div class="p-35 bg-white transation box-shadow h-100 rounded1 hover-style-1">
                             <p><i class="flaticon-quote me-1"></i>Working with Dillion Property Real Estate was a pleasure. Your professionalism and dedication to finding the right property for us were unmatched. I would recommend you to anyone looking to buy or sell property.</p>
                             <div class="agent-style-1 d-flex align-items-center mt-30">
                                 <div class="image-wrap">
@@ -38,7 +38,7 @@
                         </div>
                     </div>
                     <div class="col">
-                        <div class="p-35 bg-white transation box-shadow h-100 rounded hover-style-1">
+                        <div class="p-35 bg-white transation box-shadow h-100 rounded1 hover-style-1">
                             <p><i class="flaticon-quote me-1"></i>Thanks to Dillion's property investment plan, I've seen consistent returns on my investments. Your strategy is well-researched and tailored to maximize profitability while minimizing risks. I highly recommended them to anyone.</p>
                             <div class="agent-style-1 d-flex align-items-center mt-30">
                                 <div class="image-wrap">
